19b7ba08e02378f2e610b6d47b18100c9ba1c44c
Vojguard <jirka.kropac@gmail.com>
2024-10-11 14:41:11 +0200

    steps

--- src/MyAgent.java
+++ src/MyAgent.java
@@ -3,60 +3,59 @@ import game.core.Game;
 
 import java.util.*;
 
-public final class MyAgent extends PacManControllerBase
-{
-	private static class State {
-		Game game;
-		Integer cost;
+public final class MyAgent extends PacManControllerBase {
+    private static class State {
+        Game game;
+        Integer cost;
 
-		public State(Game game, Integer cost) {
-			this.game = game;
-			this.cost = cost;
-		}
-	}
+        public State(Game game, Integer cost) {
+            this.game = game;
+            this.cost = cost;
+        }
+    }
 
-	@Override
-	public void tick(Game game, long timeDue) {
+    @Override
+    public void tick(Game game, long timeDue) {
 
-		Queue<State> fringe = new LinkedList<>();
-		State state = new State(game, 0);
+        Queue<State> fringe = new LinkedList<>();
+        State state = new State(game, 0);
 
-		fringe.add(state);
-		HashMap<Game, Integer> visitedCosts = new HashMap<Game, Integer>() {};
+        fringe.add(state);
+        HashMap<State, Integer> visitedCosts = new HashMap<>() {
+        };
 
-		// better timeout
-		while(!fringe.isEmpty() && timeDue > 0){
-			// if goal
+        int[] directions = game.getPossiblePacManDirs(false);
+        if (directions.length == 1) {
+            pacman.set(directions[0]);
+            return;
+        }
 
-			int[] directions = game.getPossiblePacManDirs(false);
-			if (directions.length == 1){
-				pacman.set(directions[0]);
-				return;
-			} else {
-				for (int dir : game.getPossiblePacManDirs(false)){
-					Game gameCopy = game.copy();
-					gameCopy.advanceGame(dir);
-					State stateCopy = new State(gameCopy, 0);
-					//calculate cost
-					Integer gameCost = 0;
-					if (visitedCosts.get(gameCopy) != null){
-						//compare cost
-						continue;
-					}
-					visitedCosts.put(gameCopy, gameCost);
-					fringe.add(stateCopy);
-				}
-				pacman.set(directions[game.rand().nextInt(directions.length)]);
-				return;
-			}
-		}
-		if (timeDue <= 0){
-			// pacman.set best dir
-		}
-		
-	}
+        while (!fringe.isEmpty() && System.currentTimeMillis() < timeDue - 10) {
+            State current = fringe.poll();
+            // TODO: if goal
+            for (int dir : current.game.getPossiblePacManDirs(false)) {
+                State next = new State(current.game.copy(), current.cost);
+                next.game.advanceGame(dir);
+                next.cost += EvaluateState(next); //TODO: next.cost = pathCost + heuristic
+                if (visitedCosts.get(next) != null) {
+                    if (next.cost >= visitedCosts.get(next)){
+                        continue;
+                    }
+                }
+                visitedCosts.put(next, next.cost);
+                fringe.add(next);
+            }
 
-	private int EvaluateState(State stateToEval){
-		return stateToEval.cost;
-	}
+        }
+        if (System.currentTimeMillis() >= timeDue - 10) {
+            // TODO: pacman.set best dir
+        } else {
+            pacman.set(directions[game.rand().nextInt(directions.length)]);
+        }
+    }
+
+    private int EvaluateState(State stateToEval) {
+        //TODO: implement heuristic function
+        return stateToEval.cost;
+    }
 }
